e24a32cd364512cda885d14f216dc6155896642f
Norbert B <norbertbednarczyk@icloud.com>
2023-02-15 22:54:10 +0100

    Banner updated

--- pages/banner.vue
+++ pages/banner.vue
@@ -8,8 +8,11 @@ useSeoMeta({
 </script>
 
 <template>
+    <div class="top">
+        <h5>Building your <em>solutions</em></h5>
+        <p>Customized servers, storage and workstations</p>
+    </div>
     <div class="banner">
-        <!-- some background-image here -->
         <img src="/proc.png" alt="Processor image">
         <div class="texts">
             <h5>Design. Build. Accelerate. On The Ultimate Workstation Processor</h5>
@@ -17,15 +20,45 @@ useSeoMeta({
                 professional workstation.</p>
         </div>
     </div>
+    <div class="footer">
+        <p>Choose from our selection of solutions</p>
+</div>
 </template>
 
 <style lang="scss" scoped>
+.top,
+.footer {
+    text-align: center;
+    color: white;
+    background-color: #2d384c;
+    position: relative;
+    z-index: 2;
+}
+
+.top {
+    & h5 {
+        font-weight: 600;
+        font-size: 2.2rem;
+        padding: .5rem 0;
+
+        & em {
+            font-style: normal;
+            color: #68afdb;
+        }
+    }
+
+    & p {
+        padding-bottom: .5rem;
+    }
+}
+
+
+
 .banner {
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 2rem;
-    padding: 4rem;
     background-image: linear-gradient(to right, #815d19, #e99b0c 30%, #815d19);
 
     @media (min-width: $bp-sm) {
@@ -67,5 +100,10 @@ useSeoMeta({
             }
         }
     }
+}
+
+.footer p {
+    font-size: 1.8rem;
+    padding: 1rem 0;
 }
 </style>
